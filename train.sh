6019aa8d2cc72075e8451236b0a72ee5ea5f8017
Vicy <vice2city@qq.com>
2025-11-07 16:33:18 +0800

    update sh

--- train.sh
+++ train.sh
@@ -2,4 +2,4 @@ python train.py --dataroot ./datasets/opt2sar --name opt2sar_1015 --model cycle_
 
 python test.py --dataroot /mnt/hdd1/xueyijun/Vicy/20250612/data/20250924/ChangGuang/fleet/images --name opt2sar_new --model test --no_dropout --preprocess scale_width --load_size 800
 
-python test.py --dataroot /mnt/hdd1/xueyijun/Vicy/20250612/data/20250924/ChangGuang/images --name opt2sar_new --model test --no_dropout --preprocess scale_width --load_size 800 --num_test 1000
+python test.py --dataroot /app/dataroot/output/fleet/images --name opt2sar --model test --no_dropout --preprocess scale_width --load_size 2048 --num_test 1000
